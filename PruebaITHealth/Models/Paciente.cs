db308c8a461a15e5c0e9230b1498fe35fd2cb5a2
rafaelospinacastro <81756302+rafaelospinacastro@users.noreply.github.com>
2024-10-31 18:02:39 -0500

    Agregar archivos de proyecto.

---
+++ PruebaITHealth/Models/Paciente.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PruebaITHealth.Models
+{
+    public class Paciente
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string? TipoDocumento { get; set; }
+        [Required]
+        public string? NumeroDocumento { get; set; }
+        [Required]
+        public string? Nombre { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public DateTime? FechaNacimiento { get; set; }
+        [Required]
+        public string? Genero { get; set; }
+        [Required]
+        public string? Direccion { get; set; }
+        [Required]
+        public int? Telefono { get; set; }
+        [Required]
+        public bool? Activo { get; set; }
+
+
+    }
+
+}
